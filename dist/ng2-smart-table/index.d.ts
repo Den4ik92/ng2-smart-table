269fc66dca49be40a6ef6978be7f4c4d06334649
kirichenko.denis92 <kirichenko.denis92@gmail.com>
2023-08-29 18:46:44 +0300

    fixed bugs in data source

--- dist/ng2-smart-table/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@Den4ik92/ng2-smart-table" />
-export * from './public-api';
--- dist/ng2-smart-table/lib/components/thead/cells/checkbox-select-all.component.d.ts
+++
@@ -1,9 +0,0 @@
-import { Grid } from '../../../lib/grid';
-import { DataSource } from '../../../lib/data-source/data-source';
-import * as i0 from "@angular/core";
-export declare class CheckboxSelectAllComponent {
-    grid: Grid;
-    source: DataSource;
-    static ɵfac: i0.ɵɵFactoryDeclaration<CheckboxSelectAllComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<CheckboxSelectAllComponent, "[ng2-st-checkbox-select-all]", never, { "grid": "grid"; "source": "source"; }, {}, never, never, false, never>;
-}
