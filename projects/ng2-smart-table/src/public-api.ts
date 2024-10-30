760df6e3a7b8817e4a9dd0d9ddf4a973dbd79356
Kirichenko Denis <kirichenko.denis92@gmail.com>
2024-10-30 11:49:34 +0200

    update expots from public api

--- projects/ng2-smart-table/src/public-api.ts
+++ projects/ng2-smart-table/src/public-api.ts
@@ -1,9 +1,18 @@
-export { DefaultEditor, Editor } from './lib/components/cell/cell-editors/default-editor';
-export { ViewCell } from './lib/components/cell/cell-view-mode/view-cell';
-export { DefaultFilter, Filter } from './lib/components/filter/filter-types/default-filter';
-export { Cell } from './lib/lib/data-set/cell';
-export { LocalDataSource } from './lib/lib/data-source/local/local.data-source';
-export * from './lib/lib/interfaces/smart-table.models';
-export * from './lib/ng2-smart-table.component';
-export * from './lib/ng2-smart-table.module';
+export {
+  DefaultEditor,
+  Editor
+} from "./lib/components/cell/cell-editors/default-editor";
+export { ViewCell } from "./lib/components/cell/cell-view-mode/view-cell";
+export {
+  DefaultFilter,
+  Filter
+} from "./lib/components/filter/filter-types/default-filter";
+export { Cell } from "./lib/lib/data-set/cell";
+export { Column } from "./lib/lib/data-set/column";
+export { Row } from "./lib/lib/data-set/row";
+export { DataSource } from "./lib/lib/data-source/data-source";
+export { LocalDataSource } from "./lib/lib/data-source/local/local.data-source";
+export * from "./lib/lib/interfaces/smart-table.models";
+export * from "./lib/ng2-smart-table.component";
+export * from "./lib/ng2-smart-table.module";
 
