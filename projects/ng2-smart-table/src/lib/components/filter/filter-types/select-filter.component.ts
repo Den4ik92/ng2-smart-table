03ea1ae8dae7cac468c6c6b9e15df02bcf75a629
kirichenko.denis92 <kirichenko.denis92@gmail.com>
2025-04-19 01:13:22 +0300

    fix filter select reset

--- projects/ng2-smart-table/src/lib/components/filter/filter-types/select-filter.component.ts
+++ projects/ng2-smart-table/src/lib/components/filter/filter-types/select-filter.component.ts
@@ -7,7 +7,7 @@ import { BaseFilterComponent } from './base-filter.component';
   selector: 'ng2-select-filter',
   template: `
     <select [class]="inputClass()" class="form-control" [formControl]="inputControl">
-      <option value="null">{{ column().getFilterConfig().selectText || 'all' }}</option>
+      <option value="">{{ column().getFilterConfig().selectText || 'all' }}</option>
       @for (option of column().getFilterConfig().list; track $index) {
         <option [value]="option.value">
           {{ option.title }}
